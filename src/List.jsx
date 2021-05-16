5a1c2225769e6ba20a15e5d065f6c454bb248644
sushilv3 <sushilv3rma@gmail.com>
2021-05-16 13:17:56 +0530

    added List component

---
+++ src/List.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const List = (props)=>{
+
+    const[line, setLine] = useState(false);
+    const cutIt = ()=>{
+        setLine(true);
+    };
+
+
+    return (
+        <div className="todo_style">
+        <span onClick={cutIt}><DeleteIcon className="deleteIcon"/></span>
+            <li style={{textDecoration:line ? "line-through" : 'none'}}>{props.text}</li>
+        </div>
+        );
+}
+export default List;
