72dc0b3a03f1de01ca1d22d985a0447bce6746cc
sushilv3 <sushilv3rma@gmail.com>
2021-05-16 13:16:25 +0530

    deleted comment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ToDoList from './ToDoList';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
  
-    <App />
+    <ToDoList />
  ,
   document.getElementById('root')
 );
